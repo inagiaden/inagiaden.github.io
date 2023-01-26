a39089b15ea5b8ac5671f02a8edf24f0355e0b89
inagiaden <donghaoran@mail.com>
2023-01-26 22:18:12 +0800

    Site updated: 2023-01-26 22:18:12

---
+++ dist/music.js
@@ -0,0 +1,12 @@
+t ap = new APlayer({
+	    container: document.getElementById('aplayer'),
+	    audio: [
+		    	{
+				        name: 'name',      #歌名
+				        artist: 'artist',  #歌手
+				        url: 'url.mp3',    #mp3的url地址
+				        cover: 'cover.jpg' #歌的封面
+				    }
+		    	]
+});
+
